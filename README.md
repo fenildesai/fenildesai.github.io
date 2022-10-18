580ac2a1abe51a1f7f2fc5b7e6b6434e2180727a
Fenil Desai <feniludesai@yahoo.com>
2022-10-18 23:42:20 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,98 @@
-# fenildesai.github.io
-This is my Github profile page
+# Developer portfolio template
+
+<p align="center">
+  A DEVELOPER PORTFOLIO TEMPLATE.<br>
+  Full customizable and easy to use.<br>
+</p>
+
+<p align="center">
+  <img src="https://img.shields.io/badge/version-1.0.3-informational.svg">
+  <img src="https://img.shields.io/badge/licence-MIT-blue.svg">
+</p>
+
+<p align="center">
+  Created by <a href="https://guimgonzalez.business/">Guim González</a>.
+</p>
+
+🚀 Welcome to my developer portfolio template with HTML, CSS and JS. Free to use and easy to customize. Here you have a snapshot of how it looks like.
+
+![Developer portfolio snapshot - Guim González](img/snapshot.png)
+
+💻 This is a template made for developers to use it as a portfolio page. To see a live demo visit [guimg.github.io/dev-portfolio/](https://guimg.github.io/dev-portfolio/).
+
+## Usage
+
+### Profile data
+
+✏️The first step is to change the profile data. To do so, go to the file: `data/data.js`.
+
+Within the file you'll find a Javascript object, which contains all the profile data. Change the fields to your current one and save the changes.
+
+> Example
+
+This is a reduced version of the profile object:
+
+```javascript
+var profile = {
+  name: "Guim González",
+  position: "FullStack Developer & Software Engineer",
+  email: "info@guimgonzalez.business",
+  location: "Barcelona, Spain",
+  birth: "1997-07-26"
+};
+```
+
+Let's change the profile information to `John's Doe`:
+
+```javascript
+// My new profile
+var profile = {
+  name: "John Doe",
+  position: "Web Developer at Google.",
+  email: "john.doe@example.com",
+  location: "San Francisco, CA",
+  birth: "1989-09-06"
+};
+```
+
+✅ Done! Let's begin with the styling.
+
+### Theme
+
+🎨 If you don't like the color I've put some other ones. Try them and choose your favourite one.
+
+- Nightsky
+- Sunset
+- Purpledream
+- Cutekitty
+
+```javascript
+let page = {
+  theme: "nightsky"
+  // ^ themes suported:
+  //  - nightsky (default)
+  //  - sunset
+  //  - purpledream
+  //  - cutekitty
+};
+```
+
+> In this first version you can't change the language yet. I'll upgrade it asap.
+
+## Upload your page
+
+To see your page on Github pages like this: [guimg.github.io/dev-portfolio/](https://guimg.github.io/dev-portfolio/s), just follow their [guide](https://pages.github.com/), it's super simple.
+
+## Troubleshooting and help
+
+For any problem ❗️ or question ❓ of usage [send me an email](mailto:info@guimgonzalez.business) ✉️.
+
+Feel free to open a new [pull request](https://github.com/GuimG/dev-portfolio/pulls) here.
+
+## Bugs and issues
+
+For bugs 🐛 or issues ⚠️ with the template, please [open a new issue](https://github.com/GuimG/dev-portfolio/issues) here on Github.
+
+## License
+
+This code is under the [MIT License](https://opensource.org/licenses/MIT) which you can find in the [master branch](https://github.com/GuimG/dev-portfolio/blob/master/LICENSE).
---
+++ data/data.js
@@ -0,0 +1,91 @@
+/*!
+ * Guim González - Portfolio v1.0.0 (https://guimgonzalez.me)
+ * Copyright 2019 Guim González
+ * Licensed under MIT (https://github.com/GuimG/dev-portfolio/master/LICENSE)
+ */
+
+// Change your profile data here
+let profile = {
+  picture: "",
+  name: "Fenil Desai",
+  description: "Web Developer",
+  bio:
+    "I'm a Web Developer interested in Web technologies. Aspiring to become a FullStack developer <br><br>High level of web languages (ASP.NET, C#, SQL Server, Azure DevOps, Jquery, JS, HTML and CSS). I work with ASP.NET, Visual Studio 2019, .NET Core, Azure DevOps (now).",
+  email: "feniludesai@yahoo.com",
+  location: "Pune, India",
+  birth: "1984-10-30", // YYYY-MM-DD format
+  skills: [
+    "ASP.NET",
+    "C#",
+    ".NET Core",
+    "Azure DevOps",
+    "React",
+    "WebRTC",,
+    "AWS",
+    "HTML",
+    "Javascript",
+    "SQL",
+  ],
+  social: {
+    linkedin: "https://www.linkedin.com/in/fenildesai/",
+    twitter: "https://twitter.com/fenildesai",
+    github: "https://github.com/fenildesai/",
+    stackoverflow: "https://stackoverflow.com/users/4032748/dreambig",
+    dev: "https://dev.to/fenildesai"
+  },
+  experience: [
+    {
+      title: "Associate Consultant",
+      company: "Tata Consultancy Services",
+      url: "https://www.tcs.com/",
+      location: "Pune, India",
+      description:
+        "Working for one of the top UK insurer in building web applications for their quote & buy journeys.",
+      from: "Jan 2011",
+      to: "Now"
+    },
+    {
+      title: "Web Developer",
+      company: "Synechron",
+      url: "https://www.synechron.com/",
+      location: "Pune, India",
+      description:
+        "I worked in R&D Department building POCs for gaining new business/projects specifically in the Social Media space - like building Facebook & Twitter applications & also worked on building Augmented Reality applications.",
+      from: "June 2010",
+      to: "Jan 2011"
+    },
+    {
+      title: "Web Developer",
+      company: "Agribuys",
+      url: "https://www.foodlinkonline.com/",
+      location: "Pune, India",
+      description:
+        "I was part of the Product Development Team for developing & designing various modules of the live web based system of FoodlinkOnline.com - a Complete Supply chain management portal for connecting buyers and sellers of fresh food and floral products.",
+      from: "Jan 2008",
+      to: "June 2010"
+    }
+  ],
+  education: [
+    {
+      school: "South Gujarat University, Surat",
+      degree: "Masters in Computer Applications",
+      fieldOfStudy: "Computers",
+      location: "Bharuch, Gujarat, India",
+      from: "2005",
+      to: "2008"
+    }
+  ],
+  quote: "A cricketer by heart & a developer by mistake"
+};
+
+let page = {
+  language: "english",
+  // ^ languages suported:
+  //  - english (default)
+  theme: "nightsky"
+  // ^ themes suported:
+  //  - nightsky (default)
+  //  - sunset
+  //  - purpledream
+  //  - cutekitty
+};
